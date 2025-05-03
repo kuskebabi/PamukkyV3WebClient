539e66a02f87b2807d616e28d267baa03f7a9970
Hakan <103432992+HAKANKOKCU@users.noreply.github.com>
2025-05-03 19:16:14 +0300

    Leaving and kicking

--- main.js
+++ main.js
@@ -479,7 +479,7 @@ function loadmainarea() {
 															fetch(currserver + "edituser", {body: JSON.stringify({'token': logininfo.token, 'groupid': ugid, 'userid': user.user, 'role': ri.value }),method: 'POST'}).then((res) => {
 																if (res.ok) {
 																	res.text().then((text) => {
-																		
+
 																	})
 																}else {
 																	
@@ -489,6 +489,27 @@ function loadmainarea() {
 														urole.appendChild(ri);
 													}
 													urow.appendChild(urole);
+													if (crole.AllowKicking || crole.AllowBanning) {
+														let uacts = document.createElement("td");
+														if (crole.AllowKicking) {
+															let kickbtn = document.createElement("button");
+															kickbtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M640-520v-80h240v80H640Zm-280 40q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z"/></svg>';
+															kickbtn.addEventListener("click",function() {
+																if (confirm("Do you really want to kick this user?")) {
+																	fetch(currserver + "kickuser", {body: JSON.stringify({'token': logininfo.token, 'groupid': ugid, 'uid': user.user}),method: 'POST'}).then((res) => {
+																		if (res.ok) {
+																			urow.remove();
+																		}else {
+
+																		}
+																	})
+																}
+															})
+															uacts.appendChild(kickbtn);
+														}
+														urow.appendChild(uacts);
+													}
+
 													userstable.appendChild(urow);
 												})
 											});
@@ -519,9 +540,7 @@ function loadmainarea() {
 							}else {
 								fetch(currserver + "editgroup", {body: JSON.stringify({'token': logininfo.token, 'groupid': ugid, 'name': nameinp.value, 'picture': infod.picture, 'info': desinp.value, 'roles': roles  }),method: 'POST'}).then((res) => {
 									if (res.ok) {
-										res.text().then((text) => {
-											
-										})
+
 									}else {
 										
 									}
@@ -529,6 +548,21 @@ function loadmainarea() {
 							}
 						})
 						diag.inner.appendChild(savebtn);
+
+						let leavebtn = document.createElement("button");
+						leavebtn.innerText = "Leave";
+						leavebtn.addEventListener("click",function() {
+							if (confirm("Do you really want to leave this group?\nIf you are the owner, promote someone else as the owner BEFORE leaving the group.")) {
+								fetch(currserver + "leavegroup", {body: JSON.stringify({'token': logininfo.token, 'groupid': ugid }),method: 'POST'}).then((res) => {
+									if (res.ok) {
+										loadchats();
+									}else {
+
+									}
+								})
+							}
+						})
+						diag.inner.appendChild(leavebtn);
 						
 						f.onchange = function() {
 							if (f.files && f.files[0]) {
